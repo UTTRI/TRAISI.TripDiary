3d6d1a8198129b536197a6dcdfe6529f40b7b829
Brendan Reilly <brendan.benting@gmail.com>
2018-09-20 13:27:52 -0400

    updated configuration model

---
+++ src/TripDiary/timeline/models/timeline-configuration.model.ts
@@ -0,0 +1,5 @@
+export interface TimelineConfiguration
+{
+    startTime: Date;
+    endTime: Date;
+}
--- src/TripDiary/timeline/services/timeline.service.ts
+++ src/TripDiary/timeline/services/timeline.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from "@angular/core";
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs/Observable';
 import { TimelineState } from "../models/timeline-state.model";
+import { TimelineConfiguration } from "../models/timeline-configuration.model";
 
 @Injectable()
 export class TimelineService
 {
 
+    private _configuration: TimelineConfiguration;
+
+    public get configuration(): TimelineConfiguration
+    {
+        return this._configuration;
+    }
     /**
      *Creates an instance of TimelineService.
      * @param {Store<TimelineState>} store
