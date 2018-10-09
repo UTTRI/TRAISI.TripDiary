44c9c393283b7c32faa3b25ce5f95fef43c96739
Brendan Reilly <brendan.benting@gmail.com>
2018-10-09 17:14:17 -0400

    timeline type

--- src/TripDiaryController.cs
+++ src/TripDiaryController.cs
@@ -6,6 +6,10 @@ namespace TRAISI.Extensions.TripDiary
 
         private readonly TripDiaryService _tripDiaryService;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tripDiaryService"></param>
         public TripDiaryController(TripDiaryService tripDiaryService)
         {
             this._tripDiaryService = tripDiaryService;
