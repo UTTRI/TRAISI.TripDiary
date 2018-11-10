f28ca39e8b85abce040c0b2f29227a0fa5eaf7e6
Brendan Reilly <brendan.benting@gmail.com>
2018-11-10 17:19:08 -0500

    Updated timeline to use location purpose from piped locations

--- src/timeline/components/timeline/timeline.component.ts
+++ src/timeline/components/timeline/timeline.component.ts
@@ -1,30 +1,30 @@
 import {
-	Component,
-	OnInit,
-	ElementRef,
-	ComponentFactoryResolver,
-	ViewChild,
-	Inject,
-	TemplateRef,
-	ViewContainerRef,
-	Injector,
-	ContentChild,
-	AfterViewInit,
-	ContentChildren,
-	QueryList,
-	ViewChildren,
-	AfterViewChecked
+    Component,
+    OnInit,
+    ElementRef,
+    ComponentFactoryResolver,
+    ViewChild,
+    Inject,
+    TemplateRef,
+    ViewContainerRef,
+    Injector,
+    ContentChild,
+    AfterViewInit,
+    ContentChildren,
+    QueryList,
+    ViewChildren,
+    AfterViewChecked
 } from '@angular/core';
 
 import { TimelineService } from '../../services/timeline.service';
 import {
-	SurveyQuestion,
-	ResponseTypes,
-	SurveyViewer,
-	OnVisibilityChanged,
-	SurveyResponder,
-	ResponseData,
-	TimelineResponseData
+    SurveyQuestion,
+    ResponseTypes,
+    SurveyViewer,
+    OnVisibilityChanged,
+    SurveyResponder,
+    ResponseData,
+    TimelineResponseData
 } from 'traisi-question-sdk';
 import { TimelineWedgeComponent } from '../timeline-wedge/timeline-wedge.component';
 import { PopoverDirective } from 'ngx-bootstrap/popover';
@@ -36,246 +36,250 @@ import { isRegExp } from 'util';
 import { TimelineDockComponent } from '../timeline-dock/timeline-dock.component';
 
 @Component({
-	entryComponents: [TimelineWedgeComponent],
-	selector: 'traisi-timeline-question',
-	template: require('./timeline.component.html').toString(),
-	styles: [require('./timeline.component.scss').toString()],
-	providers: [TimelineService]
+    entryComponents: [TimelineWedgeComponent],
+    selector: 'traisi-timeline-question',
+    template: require('./timeline.component.html').toString(),
+    styles: [require('./timeline.component.scss').toString()],
+    providers: [TimelineService]
 })
 export class TimelineComponent extends SurveyQuestion<ResponseTypes.Timeline[]>
-	implements OnInit, AfterViewInit, AfterViewChecked, OnVisibilityChanged {
-	public editModel: TimelineEntry;
+    implements OnInit, AfterViewInit, AfterViewChecked, OnVisibilityChanged {
+    public editModel: TimelineEntry;
 
-	@ViewChild(PopoverDirective)
-	public popovers;
+    @ViewChild(PopoverDirective)
+    public popovers;
 
-	@ViewChild(TemplateRef, { read: ViewContainerRef })
-	public inputTemplate: ViewContainerRef;
+    @ViewChild(TemplateRef, { read: ViewContainerRef })
+    public inputTemplate: ViewContainerRef;
 
-	@ViewChild('newEntry')
-	public newEntryDialog: TimelineNewEntryComponent;
+    @ViewChild('newEntry')
+    public newEntryDialog: TimelineNewEntryComponent;
 
-	@ViewChild('timelineDock')
-	public timelineDock: TimelineDockComponent;
+    @ViewChild('timelineDock')
+    public timelineDock: TimelineDockComponent;
 
-	public isStep1: boolean = false;
+    public isStep1: boolean = false;
 
-	public isStep2: boolean = false;
+    public isStep2: boolean = false;
 
-	/**
-	 *
-	 * @param surveyViewerService
-	 * @param _timelineService
-	 */
-	constructor(
-		@Inject('SurveyViewerService') private surveyViewerService: SurveyViewer,
-		@Inject('SurveyResponderService') private surveyResponderService: SurveyResponder,
-		private _timelineService: TimelineService
-	) {
-		super();
+    /**
+     *
+     * @param surveyViewerService
+     * @param _timelineService
+     */
+    constructor(
+        @Inject('SurveyViewerService') private surveyViewerService: SurveyViewer,
+        @Inject('SurveyResponderService') private surveyResponderService: SurveyResponder,
+        private _timelineService: TimelineService
+    ) {
+        super();
 
-		this.isMultiPage = true;
-	}
+        this.isMultiPage = true;
+    }
 
-	/**
-	 * TRAISI life cycle called for when the question is prepared
-	 */
-	public traisiOnInit(): void {
-		this.isStep1 = true;
-		this.surveyViewerService.updateNavigationState(false);
-		this._timelineService.clearAvailableLocations();
-		this.surveyResponderService.listSurveyResponsesOfType(this.surveyId, ResponseTypes.Location).subscribe((result) => {
-			result.forEach((responses) => {
-				responses.responseValues.forEach((responseValue) => {
-					const element = responseValue;
+    /**
+     * TRAISI life cycle called for when the question is prepared
+     */
+    public traisiOnInit(): void {
+        this.isStep1 = true;
+        this.surveyViewerService.updateNavigationState(false);
+        this._timelineService.clearAvailableLocations();
+        this.surveyResponderService
+            .listSurveyResponsesOfType(this.surveyId, ResponseTypes.Location)
+            .subscribe(result => {
+                result.forEach(responses => {
 
-					let location: TimelineEntry = {
-						address: element.address,
-						latitude: element.latitude,
-						purpose: 'home',
-						longitude: element.longitude,
-						time: new Date(),
-						timeB: new Date(),
-						pipedLocation: true,
-						id: Symbol(),
-						locationType: TimelineLocationType.Undefined,
-						name: 'Prior Location'
-					};
-					location.time.setHours(0);
-					location.time.setMinutes(0);
-					this._timelineService.addShelfLocation(location);
-				});
-			});
-		});
-	}
+                    let purpose = JSON.parse(responses.configuration.purpose).id;
 
-	/**
-	 * Angular's ngOnInit
-	 */
-	public ngOnInit(): void {
-		this._timelineService.clearTimelineLocations();
-		this.savedResponse.subscribe(this.onSavedResponseData);
-	}
+                    responses.responseValues.forEach(responseValue => {
+                        const element = responseValue;
 
-	public saveNewLocation(): void {}
+                        let location: TimelineEntry = {
+                            address: element.address,
+                            latitude: element.latitude,
+                            purpose: purpose !== undefined ? purpose : 'Prior Location',
+                            longitude: element.longitude,
+                            time: new Date(),
+                            timeB: new Date(),
+                            pipedLocation: true,
+                            id: Symbol(),
+                            locationType: TimelineLocationType.Undefined,
+                            name: purpose !== undefined ? purpose : 'Prior Location',
+                        };
+                        location.time.setHours(0);
+                        location.time.setMinutes(0);
+                        this._timelineService.addShelfLocation(location);
+                    });
+                });
+            });
+    }
 
-	/**
-	 *
-	 */
-	public addNewLocation(): void {
-		this.newEntryDialog.show(this.newEntryCallback);
-	}
+    /**
+     * Angular's ngOnInit
+     */
+    public ngOnInit(): void {
+        this._timelineService.clearTimelineLocations();
+        this.savedResponse.subscribe(this.onSavedResponseData);
+    }
 
-	/**
-	 * Callback - called when the new "location" entry process has completed.
-	 * @param value
-	 */
-	public newEntryCallback = (value: any): void => {
-		this._timelineService.addShelfLocation(value);
-	};
+    public saveNewLocation(): void {}
 
-	/**
-	 *
-	 * @param type
-	 * @param $event
-	 */
-	public handler(type: string, $event: ModalDirective) {}
+    /**
+     *
+     */
+    public addNewLocation(): void {
+        this.newEntryDialog.show(this.newEntryCallback);
+    }
 
-	/**
-	 *
-	 */
-	public ngAfterViewInit(): void {
-		this.timelineDock.timelineNewEntry = this.newEntryDialog;
-	}
+    /**
+     * Callback - called when the new "location" entry process has completed.
+     * @param value
+     */
+    public newEntryCallback = (value: any): void => {
+        this._timelineService.addShelfLocation(value);
+    };
 
-	/**
-	 *
-	 */
-	public ngAfterViewChecked(): void {}
+    /**
+     *
+     * @param type
+     * @param $event
+     */
+    public handler(type: string, $event: ModalDirective) {}
 
-	/**
-	 * Navigates internal next
-	 * @returns true if there are no more internal pages
-	 */
-	public navigateInternalNext(): boolean {
-		if (this.isStep2) {
-			return true;
-		} else if (this.isStep1 && this._timelineService.isTimelineStatevalid) {
-			this.isStep1 = false;
-			this.isStep2 = true;
-			this.saveCurrentResponseState();
+    /**
+     *
+     */
+    public ngAfterViewInit(): void {
+        this.timelineDock.timelineNewEntry = this.newEntryDialog;
+    }
 
-			return false;
-		}
+    /**
+     *
+     */
+    public ngAfterViewChecked(): void {}
 
-		return false;
-	}
+    /**
+     * Navigates internal next
+     * @returns true if there are no more internal pages
+     */
+    public navigateInternalNext(): boolean {
+        if (this.isStep2) {
+            return true;
+        } else if (this.isStep1 && this._timelineService.isTimelineStatevalid) {
+            this.isStep1 = false;
+            this.isStep2 = true;
+            this.saveCurrentResponseState();
 
-	/**
-	 * Saves current response state
-	 */
-	private saveCurrentResponseState(): void {
-		this._timelineService.timelineLocations.subscribe((entries: TimelineEntry[]) => {
-			this.response.emit(entries);
-		});
-		// this.response.emit();
-	}
+            return false;
+        }
 
-	/**
-	 * Override - signfies an internal navigation possible.
-	 */
-	public canNavigateInternalNext(): boolean {
-		if (this.isStep2) {
-			return true;
-		} else {
-			return this._timelineService.isTimelineStatevalid;
-		}
-	}
+        return false;
+    }
 
-	/**
-	 * Navigates internal previous
-	 */
-	public navigateInternalPrevious() {
-		this.isStep2 = false;
-		this.isStep1 = true;
-	}
+    /**
+     * Saves current response state
+     */
+    private saveCurrentResponseState(): void {
+        this._timelineService.timelineLocations.subscribe((entries: TimelineEntry[]) => {
+            this.response.emit(entries);
+        });
+        // this.response.emit();
+    }
 
-	/**
-	 *
-	 */
-	public canNavigateInternalPrevious(): boolean {
-		return this.isStep2;
-	}
+    /**
+     * Override - signfies an internal navigation possible.
+     */
+    public canNavigateInternalNext(): boolean {
+        if (this.isStep2) {
+            return true;
+        } else {
+            return this._timelineService.isTimelineStatevalid;
+        }
+    }
 
-	/**
-	 * Determines whether question shown on
-	 */
-	public onQuestionShown(): void {
-		this._timelineService.updateLocationsValidation();
-		if (this.isStep1 && this._timelineService.isTimelineStatevalid) {
-			// this.isStep1 = false;
-			// this.isStep2 = true;
-		}
-	}
-	public onQuestionHidden(): void {}
+    /**
+     * Navigates internal previous
+     */
+    public navigateInternalPrevious() {
+        this.isStep2 = false;
+        this.isStep1 = true;
+    }
 
-	private onSavedResponseData: (response: 'none' | ResponseData<ResponseTypes.Timeline>[]) => void = (
-		response: 'none' | ResponseData<ResponseTypes.Timeline>[]
-	) => {
-		console.log(response);
-		let location: TimelineEntry = {
-			address: undefined,
-			latitude: undefined,
-			purpose: undefined,
-			longitude: undefined,
-			time: new Date(),
-			timeB: new Date(),
-			id: Symbol(),
-			pipedLocation: true,
-			locationType: TimelineLocationType.Undefined,
-			name: 'Prior Location'
-		};
-		if (response instanceof Array) {
-			if (response.length >= 1) {
-				const timelineResponse = <TimelineResponseData>response[0];
-				location.locationType = TimelineLocationType.StartLocation;
-				location.address = timelineResponse.address;
-				location.latitude = timelineResponse.latitude;
-				location.longitude = timelineResponse.longitude;
-				location.purpose = timelineResponse.purpose;
-				location.name = timelineResponse.name;
-				this._timelineService.addTimelineLocation(location);
-			}
-			if (response.length >= 2) {
-				location = Object.assign({}, location);
-				location.id = Symbol();
-				const timelineResponse = <TimelineResponseData>response[response.length - 1];
-				location.locationType = TimelineLocationType.EndLocation;
-				location.address = timelineResponse.address;
-				location.latitude = timelineResponse.latitude;
-				location.longitude = timelineResponse.longitude;
-				location.purpose = timelineResponse.purpose;
-				location.name = timelineResponse.name;
-				this._timelineService.addTimelineLocation(location);
-			}
+    /**
+     *
+     */
+    public canNavigateInternalPrevious(): boolean {
+        return this.isStep2;
+    }
 
-			if (response.length >= 3) {
-				const midResponses = response.slice(1, response.length - 1);
+    /**
+     * Determines whether question shown on
+     */
+    public onQuestionShown(): void {
+        this._timelineService.updateLocationsValidation();
+        if (this.isStep1 && this._timelineService.isTimelineStatevalid) {
+            // this.isStep1 = false;
+            // this.isStep2 = true;
+        }
+    }
+    public onQuestionHidden(): void {}
 
-				midResponses.forEach((entry) => {
-					console.log('adding mid response');
-					location = Object.assign({}, location);
-					location.id = Symbol();
-					const timelineResponse = <TimelineResponseData>entry;
-					location.locationType = TimelineLocationType.IntermediateLocation;
-					location.address = timelineResponse.address;
-					location.latitude = timelineResponse.latitude;
-					location.longitude = timelineResponse.longitude;
-					location.purpose = timelineResponse.purpose;
-					location.name = timelineResponse.name;
-					this._timelineService.addTimelineLocation(location);
-				});
-			}
-		}
-	};
+    private onSavedResponseData: (
+        response: 'none' | ResponseData<ResponseTypes.Timeline>[]
+    ) => void = (response: 'none' | ResponseData<ResponseTypes.Timeline>[]) => {
+        let location: TimelineEntry = {
+            address: undefined,
+            latitude: undefined,
+            purpose: undefined,
+            longitude: undefined,
+            time: new Date(),
+            timeB: new Date(),
+            id: Symbol(),
+            pipedLocation: true,
+            locationType: TimelineLocationType.Undefined,
+            name: 'Prior Location'
+        };
+        if (response instanceof Array) {
+            if (response.length >= 1) {
+                const timelineResponse = <TimelineResponseData>response[0];
+                location.locationType = TimelineLocationType.StartLocation;
+                location.address = timelineResponse.address;
+                location.latitude = timelineResponse.latitude;
+                location.longitude = timelineResponse.longitude;
+                location.purpose = timelineResponse.purpose;
+                location.name = timelineResponse.name;
+                this._timelineService.addTimelineLocation(location);
+            }
+            if (response.length >= 2) {
+                location = Object.assign({}, location);
+                location.id = Symbol();
+                const timelineResponse = <TimelineResponseData>response[response.length - 1];
+                location.locationType = TimelineLocationType.EndLocation;
+                location.address = timelineResponse.address;
+                location.latitude = timelineResponse.latitude;
+                location.longitude = timelineResponse.longitude;
+                location.purpose = timelineResponse.purpose;
+                location.name = timelineResponse.name;
+                this._timelineService.addTimelineLocation(location);
+            }
+
+            if (response.length >= 3) {
+                const midResponses = response.slice(1, response.length - 1);
+
+                midResponses.forEach(entry => {
+                    console.log('adding mid response');
+                    location = Object.assign({}, location);
+                    location.id = Symbol();
+                    const timelineResponse = <TimelineResponseData>entry;
+                    location.locationType = TimelineLocationType.IntermediateLocation;
+                    location.address = timelineResponse.address;
+                    location.latitude = timelineResponse.latitude;
+                    location.longitude = timelineResponse.longitude;
+                    location.purpose = timelineResponse.purpose;
+                    location.name = timelineResponse.name;
+                    this._timelineService.addTimelineLocation(location);
+                });
+            }
+        }
+    };
 }
